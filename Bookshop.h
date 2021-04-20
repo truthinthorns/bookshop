f0225f6fa389f86bb39776e6d2655b0dc9559335
truthinthorns <73923621+truthinthorns@users.noreply.github.com>
2021-04-20 15:35:57 -0500

    Update Bookshop.h

--- Bookshop.h
+++ Bookshop.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <iostream>
 #include <fstream>
 #include <vector>
 #include "Book.h"
@@ -8,15 +7,19 @@
 class Bookshop {
 
 private:
-	std::vector<Book> bookVec;
+	std::vector<Book> availableBooks;
+	std::vector<Book> rentedBooks;
 	std::string path;
 
-	void initializeBookVec(std::string path)
+	void initializeavailableBooks(std::string path)
 	{
 		std::string line;
 		std::string tempName;
-		float tempPrice = 0.0f;
+		std::string author;
 		int tempID = 0;
+		int pages;
+		float tempPrice = 0.0f;
+		bool avail = false;
 		int ctr = 0;
 
 		std::ifstream file(path);
@@ -34,16 +37,31 @@ private:
 						ctr++;
 						break;
 					case 1:
-						tempPrice = std::stof(line);
+						author = line;
 						ctr++;
 						break;
 					case 2:
 						tempID = std::stoi(line);
-						bookVec.push_back(Book(tempName, tempPrice, tempID));
+						ctr++;
+						break;
+					case 3:
+						pages = std::stoi(line);
+						ctr++;
+						break;
+					case 4:
+						tempPrice = std::stof(line);
+						ctr++;
+						break;
+					case 5:
+						avail = (line == "true");
+						availableBooks.push_back(Book(tempName, author, -1, tempID, pages, tempPrice, avail));
 						tempName.clear();
+						author.clear();
+						pages = -999;
 						tempPrice = -999;
 						tempID = -999;
 						ctr = 0;
+						avail = false;
 						break;
 					}
 				}
@@ -54,52 +72,141 @@ private:
 public:
 	Bookshop(std::string path) {
 		this->path = path;
-		initializeBookVec(this->path);
+		initializeavailableBooks(this->path);
 	};
 
 	void setPath(std::string newPath) 
 	{ 
-		bookVec.clear();
+		availableBooks.clear();
 		this->path = newPath;
-		initializeBookVec(this->path);
+		initializeavailableBooks(this->path);
 	}
 
-	int getBookVecSize() { return bookVec.size(); }
+	int getavailableBooksSize() { return availableBooks.size(); }
 
 	void printAllBooks()
 	{
-		for (auto b : bookVec)
-			std::cout << "Book Name: " << b.getBookName() << "\nBook Price: $" << b.getPrice() << "\nBook ID: " << b.getID() << std::endl << std::endl;
+		for (auto b : availableBooks)
+			if(b.getIsAvailable())
+				b.print();
 	}
 
 	void printSingleBook(int id)
 	{
-		for (Book b : bookVec)
+		for (Book b : availableBooks)
 		{
-			if (b.getID() == id)
-				std::cout << "Book Name: " << b.getBookName() << "\nBook Price: $" << b.getPrice() << "\nBook ID: " << b.getID() << std::endl << std::endl;
+			if (b.getID() == id && b.getIsAvailable())
+				b.print();
 		}
 	}
 
 	void sellBook(int id)
 	{
-		for (int i = 0; i < bookVec.size(); i++)
+		for (unsigned int i = 0; i < availableBooks.size(); i++)
 		{
-			if (bookVec.at(i).getID() == id)
+			if (availableBooks.at(i).getID() == id && availableBooks.at(i).getIsAvailable())
 			{
-				std::cout << "Successfully purchased " << bookVec.at(i).getBookName() << std::endl;
-				bookVec.erase(bookVec.begin() + i);
+				std::cout << "Successfully purchased " << availableBooks.at(i).getBookName() << std::endl;
+				availableBooks.erase(availableBooks.begin() + i);
+			}
+		}
+	}
+
+	void rentBook(int id, int rID)
+	{
+		for (unsigned int i = 0; i < availableBooks.size(); i++)
+		{
+			if (availableBooks.at(i).getID() == id && availableBooks.at(i).getIsAvailable())
+			{
+				std::cout << "Successfully Rented Out " << availableBooks.at(i).getBookName() << " to Renter " << rID << std::endl;
+				availableBooks.at(i).setRenterID(rID);
+				availableBooks.at(i).setIsAvailable(false);
+				rentedBooks.push_back(availableBooks.at(i));
+				availableBooks.erase(availableBooks.begin() + i);
+			}
+		}
+	}
+
+	void returnBook(int id, int rID)
+	{
+		if(id!=-999)
+			for(unsigned int i = 0; i < rentedBooks.size(); i++)
+			{
+				if (rentedBooks.at(i).getRenterID() == id) 
+				{
+					rentedBooks.at(i).setRenterID(rID);
+					rentedBooks.at(i).setIsAvailable(true);
+					std::cout << "Successfully Returned Renter " << rID << "'s Book: " << rentedBooks.at(i).getBookName() << std::endl;
+					availableBooks.push_back(rentedBooks.at(i));
+					rentedBooks.erase(rentedBooks.begin() + i);
+				}
+			}
+		else if (rID != -999)
+		{
+			for (unsigned int i = 0; i < rentedBooks.size(); i++)
+			{
+				if (rentedBooks.at(i).getRenterID() == rID)
+				{
+					rentedBooks.at(i).setRenterID(rID);
+					rentedBooks.at(i).setIsAvailable(true);
+					std::cout << "Successfully Returned Renter " << rID << "'s Book: " << rentedBooks.at(i).getBookName() << std::endl;
+					availableBooks.push_back(rentedBooks.at(i));
+					rentedBooks.erase(rentedBooks.begin() + i);
+					 
+				}
 			}
 		}
 	}
 
 	bool validID(int id)
 	{
-		for (Book b : bookVec)
+		for (Book b : availableBooks)
 		{
 			if (b.getID() == id)
 				return true;
 		}
+		for (Book b : rentedBooks)
+		{
+			if (b.getID() == id)
+				return true;
+		}
+
 		return false;
+	}
+
+	Book setBookRef(int id)
+	{
+		for (Book b : availableBooks)
+		{
+			if (b.getID() == id && b.getIsAvailable())
+				return Book(b.getBookName(), b.getAuthor(), -999, b.getID(), b.getPages(), b.getPrice(),true);
+		}
+		return Book();
+	}
+
+	void searchByID(int id)
+	{
+		for (Book b : availableBooks)
+		{
+			if (b.getID() == id)
+				b.print();
+		}
+	}
+
+	void searchByAuthor(std::string name)
+	{
+		std::vector<Book> rVec;
+		for (unsigned int i = 0; i < availableBooks.size(); i++)
+		{
+			if (availableBooks.at(i).getAuthor().find(name) != 4294967295)
+				rVec.push_back(availableBooks.at(i));
+		}
+		if (rVec.empty())
+			std::cout << "Your search produced no results!\n\n";
+		else
+		{
+			for (Book b : rVec)
+				printSingleBook(b.getID());
+		}
 	}
 };
